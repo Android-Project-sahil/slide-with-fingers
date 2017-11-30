bf6c5dfc5fd75aecd11d085d530e7cca8fb183ff
SAHIL VERMA <connect.sahil5@gmail.com>
2017-12-01 00:04:11 +0530

    commit

---
+++ src/com/example/slidewiththefinger/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.slidewiththefinger;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+
+public class MainActivity extends Activity {
+	private Animation animUp;
+	private Animation animDown;
+	 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }}
+
+
+    
+
